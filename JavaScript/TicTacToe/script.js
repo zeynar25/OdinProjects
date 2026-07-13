8d1ee42b628d3c488d8c01bf94a85b523e335b29
nate <ryrgnx@gmail.com>
2026-07-13 19:51:23 +0800

    GUI implemented

--- JavaScript/TicTacToe/script.js
+++ JavaScript/TicTacToe/script.js
@@ -1,99 +1,138 @@
 console.log("Welcome to TicTacToe!");
 
 const gameBoard = (() => {
-  let board;
+  let thisBoard;
 
   const initialize = () => {
-    board = ["", "", "", "", "", "", "", "", ""];
+    thisBoard = ["", "", "", "", "", "", "", "", ""];
+
+    symbolSelection.style.display = "none";
+    restartBtn.style.display = "block";
+    restartBtn.textContent = "Restart Game";
+
+    board.innerHTML = "";
+    thisBoard.forEach((symbol) => {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.textContent = symbol;
+      board.appendChild(square);
+    });
+
+    attachSquareListeners();
   };
 
-  const getGameOverStatus = () => {
+  const updateBoard = (index, symbol) => {
+    if (index < 0 || index > 8) {
+      console.log(`Invalid index: ${index}. Must be between 0 and 8.`);
+      return;
+    }
+
+    console.log(`Placing ${symbol} at index ${index}`);
+
+    thisBoard[index] = symbol;
+
+    board.innerHTML = "";
+    thisBoard.forEach((symbol) => {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.textContent = symbol;
+      board.appendChild(square);
+    });
+
+    attachSquareListeners();
+  };
+
+  const isDraw = () => {
+    return thisBoard.every((square) => square !== "");
+  };
+
+  const isGameOver = () => {
     if (
-      (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) ||
-      (board[3] !== "" && board[3] === board[4] && board[4] === board[5]) ||
-      (board[6] !== "" && board[6] === board[7] && board[7] === board[8]) ||
-      (board[0] !== "" && board[0] === board[3] && board[3] === board[6]) ||
-      (board[1] !== "" && board[1] === board[4] && board[4] === board[7]) ||
-      (board[2] !== "" && board[2] === board[5] && board[5] === board[8]) ||
-      (board[0] !== "" && board[0] === board[4] && board[4] === board[8]) ||
-      (board[2] !== "" && board[2] === board[4] && board[4] === board[6])
+      (thisBoard[0] !== "" &&
+        thisBoard[0] === thisBoard[1] &&
+        thisBoard[1] === thisBoard[2]) ||
+      (thisBoard[3] !== "" &&
+        thisBoard[3] === thisBoard[4] &&
+        thisBoard[4] === thisBoard[5]) ||
+      (thisBoard[6] !== "" &&
+        thisBoard[6] === thisBoard[7] &&
+        thisBoard[7] === thisBoard[8]) ||
+      (thisBoard[0] !== "" &&
+        thisBoard[0] === thisBoard[3] &&
+        thisBoard[3] === thisBoard[6]) ||
+      (thisBoard[1] !== "" &&
+        thisBoard[1] === thisBoard[4] &&
+        thisBoard[4] === thisBoard[7]) ||
+      (thisBoard[2] !== "" &&
+        thisBoard[2] === thisBoard[5] &&
+        thisBoard[5] === thisBoard[8]) ||
+      (thisBoard[0] !== "" &&
+        thisBoard[0] === thisBoard[4] &&
+        thisBoard[4] === thisBoard[8]) ||
+      (thisBoard[2] !== "" &&
+        thisBoard[2] === thisBoard[4] &&
+        thisBoard[4] === thisBoard[6])
     ) {
       return true;
     }
     return false;
   };
 
-  const getGameBoard = () => board;
+  const getGameBoard = () => thisBoard;
 
-  return { initialize, getGameOverStatus, getGameBoard };
+  return { initialize, updateBoard, isDraw, isGameOver, getGameBoard };
 })();
 
 const game = (() => {
   let winner;
+  let player;
+  let computer;
 
   const start = (symbol) => {
     gameBoard.initialize();
     let moves = 0;
-    const player = createPlayer("Player", symbol);
-    const computer = createPlayer("Computer", symbol === "X" ? "O" : "X");
+    player = createPlayer("Player", symbol);
+    computer = createPlayer("Computer", symbol === "X" ? "O" : "X");
 
-    let winner = null;
-    let done = false;
-    while (moves < 9 && !done) {
-      if (moves % 2 === 0) {
-        if (player.symbol === "X") {
-          const playerMove = player.makeMove();
-          player.placeSymbol(playerMove);
+    if (symbol === "O") {
+      handleMove(computer.makeRandomMove(), computer.symbol, false);
+    }
+  };
+
+  const handleMove = (index, symbol = player.symbol, playerToMove = true) => {
+    if (gameBoard.getGameBoard()[index] === "") {
+      gameBoard.updateBoard(index, symbol);
+
+      if (gameBoard.isGameOver()) {
+        if (symbol === player.symbol) {
+          winner = player.name;
         } else {
-          const computerMove = computer.makeRandomMove();
-          computer.placeSymbol(computerMove);
+          winner = computer.name;
         }
-      } else {
-        if (player.symbol === "O") {
-          const playerMove = player.makeMove();
-          player.placeSymbol(playerMove);
-        } else {
-          const computerMove = computer.makeRandomMove();
-          computer.placeSymbol(computerMove);
-        }
+
+        gameMessage.textContent = `${winner} wins!`;
+        restartBtn.textContent = "Play Again";
+        return;
       }
 
-      console.log(gameBoard.getGameBoard());
-
-      if (gameBoard.getGameOverStatus()) {
-        winner = moves % 2 === 0 ? player.name : computer.name;
-        console.log(`Game Over! ${winner} wins!`);
-        done = true;
+      if (gameBoard.isDraw()) {
+        gameMessage.textContent = "It's a draw!";
+        restartBtn.textContent = "Play Again";
+        return;
       }
 
-      moves++;
+      if (playerToMove) {
+        handleMove(computer.makeRandomMove(), computer.symbol, false);
+      }
     }
   };
 
   const getWinner = () => winner;
 
-  return { start, getWinner };
+  return { start, handleMove, getWinner };
 })();
 
 function createPlayer(name, symbol) {
-  const makeMove = () => {
-    const freeSquares = [];
-    for (let i = 0; i < 9; i++) {
-      if (gameBoard.getGameBoard()[i] === "") {
-        freeSquares.push(i);
-      }
-    }
-
-    while (freeSquares.length > 0) {
-      const position = parseInt(prompt("number 1-9"));
-      if (freeSquares.includes(position - 1)) {
-        return position - 1;
-      }
-    }
-
-    return -1;
-  };
-
   const makeRandomMove = () => {
     const freeSquares = [];
     for (let i = 0; i < 9; i++) {
@@ -110,24 +149,31 @@ function createPlayer(name, symbol) {
     return -1;
   };
 
-  const placeSymbol = (index) => {
-    const freeSquares = [];
-    for (let i = 0; i < 9; i++) {
-      if (gameBoard.getGameBoard()[i] === "") {
-        freeSquares.push(i);
-      }
-    }
-
-    if (freeSquares.includes(index)) {
-      console.log(`Placing ${symbol} at index ${index}`);
-      gameBoard.getGameBoard()[index] = symbol;
-    } else {
-      console.log("Invalid move. Please choose a free square.");
-    }
-  };
-
-  return { name, symbol, makeMove, makeRandomMove, placeSymbol };
+  return { name, symbol, makeRandomMove };
 }
 
-let symbol = "X";
-game.start(symbol);
+function attachSquareListeners() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square, index) => {
+    square.addEventListener("click", () => {
+      game.handleMove(index);
+    });
+  });
+}
+
+const symbolSelection = document.getElementById("symbolSelection");
+const board = document.getElementById("gameBoard");
+const gameMessage = document.getElementById("gameMessage");
+const restartBtn = document.getElementById("restartBtn");
+restartBtn.addEventListener("click", () => {
+  location.reload();
+});
+
+const symbol = document
+  .querySelector("#symbols")
+  .addEventListener("click", (event) => {
+    if (event.target.classList.contains("symbol")) {
+      const selectedSymbol = event.target.textContent;
+      game.start(selectedSymbol);
+    }
+  });
