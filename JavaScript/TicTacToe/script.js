8441d35bac53e02a13ed74c73629e569e674a66e
nate <ryrgnx@gmail.com>
2026-07-13 21:08:24 +0800

    separated board and message update from the gameBoard

--- JavaScript/TicTacToe/script.js
+++ JavaScript/TicTacToe/script.js
@@ -5,20 +5,6 @@ const gameBoard = (() => {
 
   const initialize = () => {
     thisBoard = ["", "", "", "", "", "", "", "", ""];
-
-    mainMenuBtn.style.display = "block";
-    restartBtn.style.display = "block";
-    restartBtn.textContent = "Restart Game";
-
-    board.innerHTML = "";
-    thisBoard.forEach((symbol) => {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      square.textContent = symbol;
-      board.appendChild(square);
-    });
-
-    attachSquareListeners();
   };
 
   const updateBoard = (index, symbol) => {
@@ -30,16 +16,6 @@ const gameBoard = (() => {
     console.log(`Placing ${symbol} at index ${index}`);
 
     thisBoard[index] = symbol;
-
-    board.innerHTML = "";
-    thisBoard.forEach((symbol) => {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      square.textContent = symbol;
-      board.appendChild(square);
-    });
-
-    attachSquareListeners();
   };
 
   const isDraw = () => {
@@ -86,9 +62,13 @@ const gameBoard = (() => {
 const gameController = (() => {
   let player;
   let computer;
+  let winner;
 
   const start = (playerName, symbol) => {
+    winner = null;
     gameBoard.initialize();
+    displayController.updateBoard(true);
+
     let moves = 0;
     player = createPlayer(playerName, symbol);
     computer = createPlayer("Computer", symbol === "X" ? "O" : "X");
@@ -99,14 +79,15 @@ const gameController = (() => {
   };
 
   const restart = () => {
+    winner = null;
     gameBoard.initialize();
-    gameMessage.textContent = "";
-    restartBtn.textContent = "Restart Game";
+    displayController.updateBoard(true);
   };
 
   const handleMove = (index, symbol = player.symbol, playerToMove = true) => {
     if (gameBoard.getGameBoard()[index] === "") {
       gameBoard.updateBoard(index, symbol);
+      displayController.updateBoard();
 
       if (gameBoard.isGameOver()) {
         if (symbol === player.symbol) {
@@ -115,16 +96,12 @@ const gameController = (() => {
           winner = computer.name;
         }
 
-        console.log(`${winner} wins!`);
-        gameMessage.textContent = `${winner} wins!`;
-        restartBtn.textContent = "Play Again";
+        displayController.declareWinner(winner);
         return;
       }
 
       if (gameBoard.isDraw()) {
-        console.log("It's a draw!");
-        gameMessage.textContent = "It's a draw!";
-        restartBtn.textContent = "Play Again";
+        displayController.declareDraw();
         return;
       }
 
@@ -137,9 +114,51 @@ const gameController = (() => {
   return { start, handleMove, restart };
 })();
 
-const displayController = () => {
-  // to be implemented.
-};
+const displayController = (() => {
+  const updateBoard = (start = false) => {
+    if (start) {
+      mainMenuBtn.style.display = "block";
+      restartBtn.style.display = "block";
+      restartBtn.textContent = "Restart Game";
+      gameMessage.textContent = "";
+    }
+
+    let board = gameBoard.getGameBoard();
+    displayBoard.innerHTML = "";
+
+    board.forEach((symbol) => {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.textContent = symbol;
+      displayBoard.appendChild(square);
+    });
+
+    attachSquareListeners();
+  };
+
+  const declareWinner = (winner) => {
+    console.log(`${winner} wins!`);
+    gameMessage.textContent = `${winner} wins!`;
+    restartBtn.textContent = "Play Again";
+  };
+
+  const declareDraw = () => {
+    console.log("It's a draw!");
+    gameMessage.textContent = "It's a draw!";
+    restartBtn.textContent = "Play Again";
+  };
+
+  function attachSquareListeners() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square, index) => {
+      square.addEventListener("click", () => {
+        gameController.handleMove(index);
+      });
+    });
+  }
+
+  return { updateBoard, declareWinner, declareDraw };
+})();
 
 function createPlayer(name, symbol) {
   const makeRandomMove = () => {
@@ -161,17 +180,8 @@ function createPlayer(name, symbol) {
   return { name, symbol, makeRandomMove };
 }
 
-function attachSquareListeners() {
-  const squares = document.querySelectorAll(".square");
-  squares.forEach((square, index) => {
-    square.addEventListener("click", () => {
-      gameController.handleMove(index);
-    });
-  });
-}
-
 const menu = document.getElementById("menu");
-const board = document.getElementById("gameBoard");
+const displayBoard = document.getElementById("gameBoard");
 const gameMessage = document.getElementById("gameMessage");
 
 const restartBtn = document.getElementById("restartBtn");
@@ -192,9 +202,9 @@ startBtn.addEventListener("click", () => {
 
   const selectedSymbol = document.querySelector(
     'input[name="symbol"]:checked',
-  ).value;
+  )?.value;
 
-  if (selectedSymbol === null) {
+  if (selectedSymbol === undefined) {
     alert("Please select a symbol (X or O) to start the game.");
     return;
   }
