14833aef66d570647950269cd13dc5d4f787020e
nate <ryrgnx@gmail.com>
2026-08-10 15:43:50 +0800

    setup completed

---
+++ JavaScript/RestaurantPage/webpack.config.js
@@ -0,0 +1,31 @@
+import path from "node:path";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
