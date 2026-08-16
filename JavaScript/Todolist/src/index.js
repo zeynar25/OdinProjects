e7d5002421cda7263fe1791269cc43ed5a4292e6
nate <ryrgnx@gmail.com>
2026-08-16 22:40:21 +0800

    feat: add project adding

--- JavaScript/Todolist/src/index.js
+++ JavaScript/Todolist/src/index.js
@@ -7,4 +7,35 @@ console.log("Todo List App");
 const projects = [new Project("Default", "The default project")];
 
 const projectNav = document.querySelector("#project-nav");
-renderNav(projectNav, projects);
+
+function refreshNav() {
+  projectNav.innerHTML = "";
+  renderNav(projectNav, projects);
+}
+
+refreshNav();
+
+const projectFormContainer = document.querySelector("#project-form-container");
+const nameInput = document.querySelector("#project-input-name");
+const addProjectBtn = document.querySelector("#add-project-btn");
+addProjectBtn.addEventListener("click", () => {
+  projectFormContainer.style.display = "flex";
+  nameInput.focus();
+});
+
+const closeFormBtn = document.querySelector("#close-form-btn");
+closeFormBtn.addEventListener("click", () => {
+  projectFormContainer.style.display = "none";
+});
+
+const projectForm = document.querySelector("#project-form");
+projectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newProject = new Project(nameInput.value);
+  projects.push(newProject);
+  refreshNav();
+
+  projectFormContainer.style.display = "none";
+  projectForm.reset();
+});
